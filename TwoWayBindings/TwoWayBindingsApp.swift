b009388cc8a19a2363addd54e18c7940c98b1a6f
Rob Breshears <robbreshears@gmail.com>
2025-09-17 14:14:16 -0600

    Initial Commit

---
+++ TwoWayBindings/TwoWayBindingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwoWayBindingsApp.swift
+//  TwoWayBindings
+//
+//  Created by Breshears, Rob - CTC on 9/17/25.
+//
+
+import SwiftUI
+
+@main
+struct TwoWayBindingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TwoWayBindingsTests/TwoWayBindingsTests.swift
@@ -0,0 +1,17 @@
+//
+//  TwoWayBindingsTests.swift
+//  TwoWayBindingsTests
+//
+//  Created by Breshears, Rob - CTC on 9/17/25.
+//
+
+import Testing
+@testable import TwoWayBindings
+
+struct TwoWayBindingsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
