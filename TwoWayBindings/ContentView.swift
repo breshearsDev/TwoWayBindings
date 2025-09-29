64c2da6e6e6e999aaebf2ea6e0cb3497f3a6cd20
Rob Breshears <robbreshears@gmail.com>
2025-09-29 14:48:58 -0600

    Here is my Codealong

--- TwoWayBindings/ContentView.swift
+++ TwoWayBindings/ContentView.swift
@@ -8,14 +8,72 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var pressCount: Int = 0
+    @State private var toggleIsOn = true
+    @State private var name = ""
+    @State private var selectedColor: Color = .red
+    @State private var selectedDate: Date = Date()
+    @State private var stepperValue = 1
+    @State private var sliderValue = 50.0
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            Spacer()
+            
+            Text("Press Count: \(pressCount)")
+            
+            Button("Press Me") {
+                pressCount += 1
+            }
+            .buttonStyle(.borderedProminent)
+            
+            Spacer()
+            
+            TextField("Enter a name", text: $name)
+                .textFieldStyle(.roundedBorder)
+            Text("Name entered: \(name)")
+            
+            Spacer()
+            
+            Toggle("Toggle is \(toggleIsOn ? "On" : "Off"): ", isOn: $toggleIsOn)
+            Spacer()
+            
+            Rectangle()
+                .fill(selectedColor)
+                .frame(width: 100, height: 100)
+            ColorPicker("Pick a Rectangle Color: ", selection: $selectedColor)
+            
+            Spacer()
+            
+            DatePicker("Date: ", selection: $selectedDate)
+            Text("Selected Date is: \(selectedDate.formatted(date: .abbreviated, time: .shortened))")
+            Spacer()
+            
+            Stepper("Stepper Value: \(stepperValue)", value: $stepperValue, in: 1...10)
+            
+            Spacer()
+            
+            
+            Slider(value: $sliderValue, in: 0...100) {
+                //
+            } minimumValueLabel: {
+                Image(systemName: "speaker.minus")
+                
+            } maximumValueLabel: {
+                Image(systemName: "speaker.plus")
+            }
+        
+            Text("Slider value: \(Int(sliderValue))")
+            Text("Slider value: \(sliderValue.formatted(.number.precision(.fractionLength(2))))")
+            Text("Slider value: \(String(format: "%.2f", sliderValue))")
+            
+            Spacer()
         }
+        .font(.title2)
         .padding()
+        
     }
 }
 
